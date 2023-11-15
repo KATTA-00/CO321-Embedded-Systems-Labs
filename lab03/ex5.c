22f90f34e528f4581e481e1bf4d97aea13cf8add
hirushig <115539987+hirushig@users.noreply.github.com>
2023-11-15 14:35:00 +0530

    ex5.0

--- lab03/ex5.c
+++ lab03/ex5.c
@@ -1,38 +1,41 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <util/delay.h>
 
-#define COUNT_LIMIT 25
-#define LED_PIN 5
+#define DELAY 100
 
-int count  = 0;
+int counter = 0;
 
 int main(void){
-    DDRB = DDRB | (1<<LED_PIN);   //Set output pin 5
+    DDRB = 0x01;   //Set output pin
 
-    TCNT0 = 6;    //Initial counter value
+    TCNT0 = 0;    //Initial counter value
 
     TCCR0A = 0x00;  //Normal Mode
     TCCR0B = 0x04;  //Prescalar 256
 
-    TIMSK0 |= (1<<TOIE0);   //Enable Time Interrupt Mask Register
+    TIMSK0|= (1<<TOIE0);   //Enable Time Interrupt Mask Register
 
     sei();  //Enable global interrupts
 
-    PORTB = PORTB | (1<<LED_PIN);
-    count = 0;
+    PORTB = 0;
+
 
     while(1){
     }
-
 }
 
 ISR(TIMER0_OVF_vect){
 
-    if (count == COUNT_LIMIT){
-        PORTB = PORTB ^ (1<<LED_PIN);
-        count = 0;
-    }else
-        count++;
+    if (counter == 24){     //256 * 24 = 6144
+        TCNT0 = 150;        //Balance  = 6250 - 6144 = 106 --> Initial Count = 256 -106 = 150
+        counter++;
+    }else if (counter == 25){   //Reset to 0
+        PORTB = PORTB ^ (1<<0);
+        TCNT0 = 0;
+        counter = 0;
+    }else if (counter < 24){
+        counter++;
+    }
 
-    TCNT0 = 6;    //Initial timer0 value
 }
