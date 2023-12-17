2a10c3882f8023fbf1b72f2fb86bdf69478238d9
Kanishka Gunawardana <kanishkagunawarthana@gmail.com>
2023-12-17 18:30:03 +0530

    Add timer and interrupt functionality to answer.c

--- mid-practice/answer.c
+++ mid-practice/answer.c
@@ -13,29 +13,32 @@ int first_step = 0;
 
 int main(void)
 {
+    // define outputs
     DDRB = 0x3F;
     PORTB = 0x02;
-
+    // define inputs
     DDRD &= ~(1 << PIN);
 
+    // set timer 0 to normal mode
     TCCR0A = 0x00;
     TCCR0B = 0x00;
+    // set the timer 0 to 256 pre scaler
     TCCR0B |= (1 << CS02);
-
+    // initialize the timer 0 register
     TCNT0 = 6;
 
+    // enable the timer 0 interrupt
     TIMSK0 |= (1 << TOIE0);
 
     EICRA = 0x00;
+    // set the INT01 to negative edge interrupt
     EICRA |= (1 << ISC01);
 
+    // enable the INT01
     EIMSK |= (1 << INT0);
 
+    // enable the global iterrupt
     sei();
-
-    TCCR2A = 0x00;
-    TCCR2A |= (1 << WGM21) | (1 << WGM20) | (1 << COM2A1);
-    TCCR2B = 0x01;
 
     OCR2A = 255;
     int val = 1;
@@ -101,6 +104,7 @@ ISR(INT0_vect)
     // {
     //     TCCR0B |= (1 << CS02);
     // }
+
     TIMSK0 ^= (1 << TOIE0);
 
     if (stop == 0)
